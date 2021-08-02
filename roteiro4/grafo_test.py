4c3daa7ad621b2e91cd95c6ced7e395667ebb950
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-08-02 11:02:39 -0300

    começando roteiro 4

---
+++ roteiro4/grafo_test.py
@@ -0,0 +1,362 @@
+import unittest
+from bibgrafo.grafo_exceptions import *
+from meu_grafo_matriz_adjacencia_nao_dir import *
+
+class TestGrafo(unittest.TestCase):
+
+    def setUp(self):
+        # Grafo da Paraíba
+        self.g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p.adicionaAresta('a1', 'J', 'C')
+        self.g_p.adicionaAresta('a2', 'C', 'E')
+        self.g_p.adicionaAresta('a3', 'C', 'E')
+        self.g_p.adicionaAresta('a4', 'P', 'C')
+        self.g_p.adicionaAresta('a5', 'P', 'C')
+        self.g_p.adicionaAresta('a6', 'T', 'C')
+        self.g_p.adicionaAresta('a7', 'M', 'C')
+        self.g_p.adicionaAresta('a8', 'M', 'T')
+        self.g_p.adicionaAresta('a9', 'T', 'Z')
+
+        # Grafo da Paraíba DFS
+        self.g_p_dfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dfs_J.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dfs_J.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dfs_J.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dfs_J.adicionaAresta('a6', 'T', 'C')
+        self.g_p_dfs_J.adicionaAresta('a8', 'M', 'T')
+        self.g_p_dfs_J.adicionaAresta('a9', 'T', 'Z')
+
+        self.g_p_dfs_C = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dfs_C.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dfs_C.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dfs_C.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dfs_C.adicionaAresta('a6', 'T', 'C')
+        self.g_p_dfs_C.adicionaAresta('a8', 'M', 'T')
+        self.g_p_dfs_C.adicionaAresta('a9', 'T', 'Z')
+
+        self.g_p_dfs_Z = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dfs_Z.adicionaAresta('a9', 'T', 'Z')
+        self.g_p_dfs_Z.adicionaAresta('a6', 'T', 'C')
+        self.g_p_dfs_Z.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dfs_Z.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dfs_Z.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dfs_Z.adicionaAresta('a7', 'M', 'C')
+
+        self.g_p_dfs_T = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dfs_T.adicionaAresta('a6', 'T', 'C')
+        self.g_p_dfs_T.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dfs_T.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dfs_T.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dfs_T.adicionaAresta('a7', 'M', 'C')
+        self.g_p_dfs_T.adicionaAresta('a9', 'T', 'Z')
+
+        # Grafo da Paraíba BFS
+        self.g_p_bfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_bfs_J.adicionaAresta('a1', 'J', 'C')
+        self.g_p_bfs_J.adicionaAresta('a2', 'C', 'E')
+        self.g_p_bfs_J.adicionaAresta('a4', 'P', 'C')
+        self.g_p_bfs_J.adicionaAresta('a6', 'T', 'C')
+        self.g_p_bfs_J.adicionaAresta('a7', 'M', 'C')
+        self.g_p_bfs_J.adicionaAresta('a9', 'T', 'Z')
+
+        self.g_p_bfs_T = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_bfs_T.adicionaAresta('a6', 'T', 'C')
+        self.g_p_bfs_T.adicionaAresta('a8', 'M', 'T')
+        self.g_p_bfs_T.adicionaAresta('a9', 'T', 'Z')
+        self.g_p_bfs_T.adicionaAresta('a1', 'J', 'C')
+        self.g_p_bfs_T.adicionaAresta('a2', 'C', 'E')
+        self.g_p_bfs_T.adicionaAresta('a4', 'P', 'C')
+
+        self.g_p_bfs_Z = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_bfs_Z.adicionaAresta('a9', 'T', 'Z')
+        self.g_p_bfs_Z.adicionaAresta('a6', 'T', 'C')
+        self.g_p_bfs_Z.adicionaAresta('a8', 'M', 'T')
+        self.g_p_bfs_Z.adicionaAresta('a1', 'J', 'C')
+        self.g_p_bfs_Z.adicionaAresta('a2', 'C', 'E')
+        self.g_p_bfs_Z.adicionaAresta('a4', 'P', 'C')
+
+        self.g_p_bfs_P = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_bfs_P.adicionaAresta('a4', 'P', 'C')
+        self.g_p_bfs_P.adicionaAresta('a1', 'J', 'C')
+        self.g_p_bfs_P.adicionaAresta('a2', 'C', 'E')
+        self.g_p_bfs_P.adicionaAresta('a6', 'T', 'C')
+        self.g_p_bfs_P.adicionaAresta('a7', 'M', 'C')
+        self.g_p_bfs_P.adicionaAresta('a9', 'T', 'Z')
+
+
+        # Grafo da Paraíba sem arestas paralelas
+        self.g_p_sem_paralelas = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_sem_paralelas.adicionaAresta('a1', 'J', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a2', 'C', 'E')
+        self.g_p_sem_paralelas.adicionaAresta('a3', 'P', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a4', 'T', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a5', 'M', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a6', 'M', 'T')
+        self.g_p_sem_paralelas.adicionaAresta('a7', 'T', 'Z')
+
+        # Grafos completos
+        self.g_c = MeuGrafo(['J', 'C', 'E', 'P'])
+        self.g_c.adicionaAresta('a1','J','C')
+        self.g_c.adicionaAresta('a2', 'J', 'E')
+        self.g_c.adicionaAresta('a3', 'J', 'P')
+        self.g_c.adicionaAresta('a4', 'E', 'C')
+        self.g_c.adicionaAresta('a5', 'P', 'C')
+        self.g_c.adicionaAresta('a6', 'P', 'E')
+
+        self.g_c2 = MeuGrafo(['Nina', 'Maria'])
+        self.g_c2.adicionaAresta('amiga', 'Nina', 'Maria')
+
+        self.g_c3 = MeuGrafo(['J'])
+
+        # Grafos com laco
+        self.g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l1.adicionaAresta('a1', 'A', 'A')
+        self.g_l1.adicionaAresta('a2', 'A', 'B')
+        self.g_l1.adicionaAresta('a3', 'A', 'A')
+
+        self.g_l2 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l2.adicionaAresta('a1', 'A', 'B')
+        self.g_l2.adicionaAresta('a2', 'B', 'B')
+        self.g_l2.adicionaAresta('a3', 'B', 'A')
+
+        self.g_l3 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l3.adicionaAresta('a1', 'C', 'A')
+        self.g_l3.adicionaAresta('a2', 'C', 'C')
+        self.g_l3.adicionaAresta('a3', 'D', 'D')
+        self.g_l3.adicionaAresta('a4', 'D', 'D')
+
+        self.g_l4 = MeuGrafo(['D'])
+        self.g_l4.adicionaAresta('a1', 'D', 'D')
+
+        self.g_l5 = MeuGrafo(['C', 'D'])
+        self.g_l5.adicionaAresta('a1', 'D', 'C')
+        self.g_l5.adicionaAresta('a2', 'C', 'C')
+
+        # Grafos desconexos
+        self.g_d = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_d.adicionaAresta('asd', 'A', 'B')
+
+        self.g_v1 = MeuGrafo(['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8'])
+        self.g_v1.adicionaAresta('a1', 'V1', 'V2')
+        self.g_v1.adicionaAresta('a2', 'V2', 'V3')
+        self.g_v1.adicionaAresta('a3', 'V3', 'V4')
+        self.g_v1.adicionaAresta('a4', 'V4', 'V5')
+        self.g_v1.adicionaAresta('a5', 'V5', 'V6')
+        self.g_v1.adicionaAresta('a6', 'V7', 'V8')
+
+        # Grafo NLW
+        self.nlw = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw.adicionaAresta('1', 'A', 'B')
+        self.nlw.adicionaAresta('2', 'A', 'G')
+        self.nlw.adicionaAresta('3', 'A', 'J')
+        self.nlw.adicionaAresta('4', 'K', 'G')
+        self.nlw.adicionaAresta('5', 'K', 'J')
+        self.nlw.adicionaAresta('6', 'J', 'G')
+        self.nlw.adicionaAresta('7', 'J', 'I')
+        self.nlw.adicionaAresta('8', 'I', 'G')
+        self.nlw.adicionaAresta('9', 'G', 'H')
+        self.nlw.adicionaAresta('10', 'H', 'F')
+        self.nlw.adicionaAresta('11', 'F', 'B')
+        self.nlw.adicionaAresta('12', 'G', 'B')
+        self.nlw.adicionaAresta('13', 'B', 'C')
+        self.nlw.adicionaAresta('14', 'C', 'D')
+        self.nlw.adicionaAresta('15', 'D', 'E')
+        self.nlw.adicionaAresta('16', 'B', 'D')
+        self.nlw.adicionaAresta('17', 'B', 'E')
+
+        #Arvore DFS
+
+        self.nlw_dfs_A = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_dfs_A.adicionaAresta('1', 'A', 'B')
+        self.nlw_dfs_A.adicionaAresta('11', 'B', 'F')
+        self.nlw_dfs_A.adicionaAresta('10', 'F', 'H')
+        self.nlw_dfs_A.adicionaAresta('9', 'H', 'G')
+        self.nlw_dfs_A.adicionaAresta('4', 'G', 'K')
+        self.nlw_dfs_A.adicionaAresta('5', 'K', 'J')
+        self.nlw_dfs_A.adicionaAresta('7', 'J', 'I')
+        self.nlw_dfs_A.adicionaAresta('13', 'B', 'C')
+        self.nlw_dfs_A.adicionaAresta('14', 'C', 'D')
+        self.nlw_dfs_A.adicionaAresta('15', 'D', 'E')
+
+        self.nlw_dfs_F = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_dfs_F.adicionaAresta('10', 'F', 'H')
+        self.nlw_dfs_F.adicionaAresta('9', 'H', 'G')
+        self.nlw_dfs_F.adicionaAresta('2', 'G', 'A')
+        self.nlw_dfs_F.adicionaAresta('1', 'A', 'B')
+        self.nlw_dfs_F.adicionaAresta('13', 'B', 'C')
+        self.nlw_dfs_F.adicionaAresta('14', 'C', 'D')
+        self.nlw_dfs_F.adicionaAresta('15', 'D', 'E')
+        self.nlw_dfs_F.adicionaAresta('3', 'A', 'J')
+        self.nlw_dfs_F.adicionaAresta('5', 'J', 'K')
+        self.nlw_dfs_F.adicionaAresta('7', 'J', 'I')
+
+        self.nlw_dfs_K = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_dfs_K.adicionaAresta('4', 'K', 'G')
+        self.nlw_dfs_K.adicionaAresta('2', 'G', 'A')
+        self.nlw_dfs_K.adicionaAresta('1', 'A', 'B')
+        self.nlw_dfs_K.adicionaAresta('11', 'B', 'F')
+        self.nlw_dfs_K.adicionaAresta('10', 'F', 'H')
+        self.nlw_dfs_K.adicionaAresta('13', 'B', 'C')
+        self.nlw_dfs_K.adicionaAresta('14', 'C', 'D')
+        self.nlw_dfs_K.adicionaAresta('15', 'D', 'E')
+        self.nlw_dfs_K.adicionaAresta('3', 'A', 'J')
+        self.nlw_dfs_K.adicionaAresta('7', 'J', 'I')
+       
+        self.nlw_dfs_C = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_dfs_C.adicionaAresta('13', 'C', 'B')
+        self.nlw_dfs_C.adicionaAresta('1', 'B', 'A')
+        self.nlw_dfs_C.adicionaAresta('2', 'A', 'G')
+        self.nlw_dfs_C.adicionaAresta('4', 'G', 'K')
+        self.nlw_dfs_C.adicionaAresta('5', 'K', 'J')
+        self.nlw_dfs_C.adicionaAresta('7', 'J', 'I')
+        self.nlw_dfs_C.adicionaAresta('9', 'G', 'H')
+        self.nlw_dfs_C.adicionaAresta('10', 'H', 'F')
+        self.nlw_dfs_C.adicionaAresta('16', 'B', 'D')
+        self.nlw_dfs_C.adicionaAresta('15', 'D', 'E')
+
+        #Arvore BFS
+
+        self.nlw_bfs_A = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_bfs_A.adicionaAresta('1', 'A', 'B')
+        self.nlw_bfs_A.adicionaAresta('2', 'A', 'G')
+        self.nlw_bfs_A.adicionaAresta('3', 'A', 'J')
+        self.nlw_bfs_A.adicionaAresta('11', 'F', 'B')
+        self.nlw_bfs_A.adicionaAresta('13', 'B', 'C')
+        self.nlw_bfs_A.adicionaAresta('16', 'B', 'D')
+        self.nlw_bfs_A.adicionaAresta('17', 'B', 'E')
+        self.nlw_bfs_A.adicionaAresta('4', 'K', 'G')
+        self.nlw_bfs_A.adicionaAresta('8', 'I', 'G')
+        self.nlw_bfs_A.adicionaAresta('9', 'G', 'H')
+
+        self.nlw_bfs_F = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_bfs_F.adicionaAresta('10', 'H', 'F')
+        self.nlw_bfs_F.adicionaAresta('11', 'F', 'B')
+        self.nlw_bfs_F.adicionaAresta('9', 'G', 'H')
+        self.nlw_bfs_F.adicionaAresta('1', 'A', 'B')
+        self.nlw_bfs_F.adicionaAresta('13', 'B', 'C')
+        self.nlw_bfs_F.adicionaAresta('16', 'B', 'D')
+        self.nlw_bfs_F.adicionaAresta('17', 'B', 'E')
+        self.nlw_bfs_F.adicionaAresta('4', 'K', 'G')
+        self.nlw_bfs_F.adicionaAresta('6', 'J', 'G')
+        self.nlw_bfs_F.adicionaAresta('8', 'I', 'G')
+
+        self.nlw_bfs_K = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_bfs_K.adicionaAresta('4', 'K', 'G')
+        self.nlw_bfs_K.adicionaAresta('5', 'K', 'J')
+        self.nlw_bfs_K.adicionaAresta('2', 'A', 'G')
+        self.nlw_bfs_K.adicionaAresta('8', 'I', 'G')
+        self.nlw_bfs_K.adicionaAresta('9', 'G', 'H')
+        self.nlw_bfs_K.adicionaAresta('12', 'G', 'B')
+        self.nlw_bfs_K.adicionaAresta('10', 'H', 'F')
+        self.nlw_bfs_K.adicionaAresta('13', 'B', 'C')
+        self.nlw_bfs_K.adicionaAresta('16', 'B', 'D')
+        self.nlw_bfs_K.adicionaAresta('17', 'B', 'E')
+
+        self.nlw_bfs_C = MeuGrafo(['K', 'J', 'G', 'H', 'F', 'B', 'C', 'D', 'E', 'A', 'I'])
+
+        self.nlw_bfs_C.adicionaAresta('13', 'B', 'C')
+        self.nlw_bfs_C.adicionaAresta('14', 'C', 'D')
+        self.nlw_bfs_C.adicionaAresta('1', 'A', 'B')
+        self.nlw_bfs_C.adicionaAresta('11', 'F', 'B')
+        self.nlw_bfs_C.adicionaAresta('12', 'G', 'B')
+        self.nlw_bfs_C.adicionaAresta('17', 'B', 'E')
+        self.nlw_bfs_C.adicionaAresta('3', 'A', 'J')
+        self.nlw_bfs_C.adicionaAresta('10', 'H', 'F')
+        self.nlw_bfs_C.adicionaAresta('4', 'K', 'G')
+        self.nlw_bfs_C.adicionaAresta('8', 'I', 'G')
+
+
+    def test_adiciona_aresta(self):
+        self.assertTrue(self.g_p.adicionaAresta('a10', 'J', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.assertTrue(self.g_p.adicionaAresta('b1', '', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.assertTrue(self.g_p.adicionaAresta('b1', 'A', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('aa-bb')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('x', 'J', 'V')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('a1', 'J', 'C')
+
+    def test_vertices_nao_adjacentes(self):
+        self.assertEqual(self.g_p.vertices_nao_adjacentes(), ['J-E', 'J-P', 'J-M', 'J-T', 'J-Z', 'C-Z', 'E-P', 'E-M', 'E-T', 'E-Z', 'P-M', 'P-T', 'P-Z', 'M-Z'])
+        self.assertEqual(self.g_c.vertices_nao_adjacentes(), [])
+        self.assertEqual(self.g_c3.vertices_nao_adjacentes(), [])
+
+    def test_ha_laco(self):
+        self.assertFalse(self.g_p.ha_laco())
+        self.assertFalse(self.g_p_sem_paralelas.ha_laco())
+        self.assertFalse(self.g_c2.ha_laco())
+        self.assertTrue(self.g_l1.ha_laco())
+        self.assertTrue(self.g_l2.ha_laco())
+        self.assertTrue(self.g_l3.ha_laco())
+        self.assertTrue(self.g_l4.ha_laco())
+        self.assertTrue(self.g_l5.ha_laco())
+
+    def test_grau(self):
+        # Paraíba
+        self.assertEqual(self.g_p.grau('J'), 1)
+        self.assertEqual(self.g_p.grau('C'), 7)
+        self.assertEqual(self.g_p.grau('E'), 2)
+        self.assertEqual(self.g_p.grau('P'), 2)
+        self.assertEqual(self.g_p.grau('M'), 2)
+        self.assertEqual(self.g_p.grau('T'), 3)
+        self.assertEqual(self.g_p.grau('Z'), 1)
+        with self.assertRaises(VerticeInvalidoException):
+            self.assertEqual(self.g_p.grau('G'), 5)
+
+        self.assertEqual(self.g_d.grau('A'), 1)
+        self.assertEqual(self.g_d.grau('C'), 0)
+        self.assertNotEqual(self.g_d.grau('D'), 2)
+
+        # Completos
+        self.assertEqual(self.g_c.grau('J'), 3)
+        self.assertEqual(self.g_c.grau('C'), 3)
+        self.assertEqual(self.g_c.grau('E'), 3)
+        self.assertEqual(self.g_c.grau('P'), 3)
+
+        # Com laço. Lembrando que cada laço conta 2 vezes por vértice para cálculo do grau
+        self.assertEqual(self.g_l1.grau('A'), 5)
+        self.assertEqual(self.g_l2.grau('B'), 4)
+        self.assertEqual(self.g_l4.grau('D'), 2)
+
+    def test_ha_paralelas(self):
+        self.assertTrue(self.g_p.ha_paralelas())
+        self.assertFalse(self.g_p_sem_paralelas.ha_paralelas())
+        self.assertFalse(self.g_c.ha_paralelas())
+        self.assertFalse(self.g_c2.ha_paralelas())
+        self.assertFalse(self.g_c3.ha_paralelas())
+        self.assertTrue(self.g_l1.ha_paralelas())
+
+    def test_arestas_sobre_vertice(self):
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('J')), set(['a1']))
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('C')), set(['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7']))
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('M')), set(['a7', 'a8']))
+        self.assertEqual(set(self.g_l2.arestas_sobre_vertice('B')), set(['a1', 'a2', 'a3']))
+        self.assertEqual(set(self.g_d.arestas_sobre_vertice('C')), set())
+        self.assertEqual(set(self.g_d.arestas_sobre_vertice('A')), set(['asd']))
+        with self.assertRaises(VerticeInvalidoException):
+            self.g_p.arestas_sobre_vertice('A')
+
+    def test_eh_completo(self):
+        self.assertFalse(self.g_p.eh_completo())
+        self.assertFalse((self.g_p_sem_paralelas.eh_completo()))
+        self.assertTrue((self.g_c.eh_completo()))
+        self.assertTrue((self.g_c2.eh_completo()))
+        self.assertTrue((self.g_c3.eh_completo()))
+        self.assertFalse((self.g_l1.eh_completo()))
+        self.assertFalse((self.g_l2.eh_completo()))
+        self.assertFalse((self.g_l3.eh_completo()))
+        self.assertFalse((self.g_l4.eh_completo()))
+        self.assertFalse((self.g_l5.eh_completo()))
