bb5dcb7fa7ca0033eaa1f050f2ce2d36d3f97a7c
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-08-02 22:44:09 -0300

    add: roteiro 4

--- roteiro4/meu_grafo_matriz_adjacencia_nao_dir.py
+++ roteiro4/meu_grafo_matriz_adjacencia_nao_dir.py
@@ -5,63 +5,88 @@ from bibgrafo.grafo_exceptions import *
 class MeuGrafo(GrafoMatrizAdjacenciaNaoDirecionado):
 
     def vertices_nao_adjacentes(self):
-        '''
-        Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
-        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
-        :return: Uma lista com os pares de vértices não adjacentes
-        '''
-        pass
+      '''
+      Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
+      Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+      :return: Uma lista com os pares de vértices não adjacentes
+      '''
+      pass
 
     def ha_laco(self):
-        '''
-        Verifica se existe algum laço no grafo.
-        :return: Um valor booleano que indica se existe algum laço.
-        '''
-        pass
-
+      '''
+      Verifica se existe algum laço no grafo.
+      :return: Um valor booleano que indica se existe algum laço.
+      '''
+      for i in range(len(self.M)):
+        if len(self.M[i][i]) > 0:
+          return True
+      
+      return False
 
     def grau(self, V=''):
-        '''
-        Provê o grau do vértice passado como parâmetro
-        :param V: O rótulo do vértice a ser analisado
-        :return: Um valor inteiro que indica o grau do vértice
-        :raises: VerticeInvalidoException se o vértice não existe no grafo
-        '''
-        if v not in self.N:
-          raise VerticeInvalidoException("O vértice não existe no grafo")
+      '''
+      Provê o grau do vértice passado como parâmetro
+      :param V: O rótulo do vértice a ser analisado
+      :return: Um valor inteiro que indica o grau do vértice
+      :raises: VerticeInvalidoException se o vértice não existe no grafo
+      '''
+      if V not in self.N:
+        raise VerticeInvalidoException("O vértice não existe no grafo")
 
-        indice_vertice = self.N.index(v)
-        grau = 0
+      indice_vertice = self.N.index(V)
+      grau = 0
 
-        for i in range(indice_vertice, len(self.M)):
-          if indice_vertice == i:
-            grau += len(self.M[indice_vertice][j])*2
-          else:
-            grau += len(self.M[indice_vertice][j])
+      for i in range(indice_vertice, len(self.M)):
+        if indice_vertice == i:
+          grau += len(self.M[indice_vertice][i])*2
+        else:
+          grau += len(self.M[indice_vertice][i])
 
-        for i in range(indice_vertice):
-          grau += len(self.M[indice_vertice][j])
-        return grau
+      for i in range(indice_vertice):
+        grau += len(self.M[i][indice_vertice])
+      return grau
 
     def ha_paralelas(self):
-        '''
-        Verifica se há arestas paralelas no grafo
-        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
-        '''
-        pass
+      '''
+      Verifica se há arestas paralelas no grafo
+      :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+      '''
+      for i in range(len(self.M)):
+        for j in range(len(self.M)):
+          if len(self.M[i][j]) >= 2:
+            return True
+          if len(self.M[i][i]) >= 2:
+            return True
+          if len(self.M[j][j]) >= 2:
+            return True
+
+      return False
 
     def arestas_sobre_vertice(self, V):
-        '''
-        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
-        :param V: O vértice a ser analisado
-        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
-        :raises: VerticeInvalidoException se o vértice não existe no grafo
-        '''
-        pass
+      '''
+      Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+      :param V: O vértice a ser analisado
+      :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+      :raises: VerticeInvalidoException se o vértice não existe no grafo
+      '''
+      pass
 
     def eh_completo(self):
-        '''
-        Verifica se o grafo é completo.
-        :return: Um valor booleano que indica se o grafo é completo
-        '''
-        pass
+      '''
+      Verifica se o grafo é completo.
+      :return: Um valor booleano que indica se o grafo é completo
+      '''
+      if self.ha_laco():
+            return False
+
+      if self.ha_paralelas():
+            return False
+
+      quantidade_de_vertices = len(self.N)
+
+      for v in self.N:
+        grau = self.grau(v)
+
+        if grau != quantidade_de_vertices-1:
+          return False  
+      return True
--- roteiro4/teste.py
+++ roteiro4/teste.py
@@ -1,13 +1,17 @@
 from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
 from meu_grafo_matriz_adjacencia_nao_dir import *
 
-g_p_dfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
-g_p_dfs_J.adicionaAresta('a1', 'J', 'C')
-g_p_dfs_J.adicionaAresta('a2', 'C', 'E')
-g_p_dfs_J.adicionaAresta('a4', 'P', 'C')
-g_p_dfs_J.adicionaAresta('a6', 'T', 'C')
-g_p_dfs_J.adicionaAresta('a8', 'M', 'T')
-g_p_dfs_J.adicionaAresta('a9', 'T', 'Z')
+g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+g_p.adicionaAresta('a1', 'J', 'C')
+g_p.adicionaAresta('a2', 'C', 'E')
+g_p.adicionaAresta('a3', 'C', 'E')
+g_p.adicionaAresta('a4', 'P', 'C') #
+g_p.adicionaAresta('a5', 'P', 'C') #
+g_p.adicionaAresta('a6', 'T', 'C')
+g_p.adicionaAresta('a7', 'M', 'C')
+g_p.adicionaAresta('a8', 'M', 'T')
+g_p.adicionaAresta('a9', 'T', 'Z')
 
-print(g_p_dfs_J)
+print(g_p)
+print(g_p.eh_completo())
 
