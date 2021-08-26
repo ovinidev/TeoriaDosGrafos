e9c46e72c3d21706e409956879692b444a57b3eb
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-08-26 10:19:45 -0300

    finishing script 6

--- roteiro6/meu_grafo_matriz_adjacencia_dir.py
+++ roteiro6/meu_grafo_matriz_adjacencia_dir.py
@@ -5,28 +5,41 @@ from copy import deepcopy
 
 class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
 
-  def warshall(self):
+  def geraMatriz(self):
+    '''
+    Provê uma matriz do grafo contendo 0 e 1 de acordo com a presença de arestas
+    :return: Uma matriz do grafo
+    '''
 
-    matriz = []
+    matriz_vazia = []
 
-    for l in range(len(self.M)):
+    for lin in range(len(self.M)):
       linha = []
-      for c in range(len(self.M)):
-        if (len(self.M[l][c]) > 0):
+      for col in range(len(self.M)):
+        if (len(self.M[lin][col]) != 0):
           linha.append(1)
         else:
           linha.append(0)
-      matriz.append(linha)
+      matriz_vazia.append(linha)
+
+    return matriz_vazia
+
+  def warshall(self):
+    '''
+    O algorítmo irá determinar a matriz de alcançabilidade (Rij) de um grafo.
+    :return: Uma matriz do grafo após o algorítmo de warshall.
+    '''
+
+    matriz = self.geraMatriz()
 
     for i in range(len(matriz)):
       for j in range(len(matriz)):
         if matriz[j][i] == 1:
           for k in range(len(matriz)):
-            jk = matriz[j][k]
-            ik = matriz[i][k]
-            if jk >= ik:
-              matriz[j][k] = jk
-            elif ik > jk:
-              matriz[j][k] = ik
+            if matriz[j][k] >= matriz[i][k]:
+              matriz[j][k] = matriz[j][k]
+            elif matriz[i][k] > matriz[j][k]:
+              matriz[j][k] = matriz[i][k]
 
     return matriz
+
