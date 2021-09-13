96dc40df3fe139e2cd65775ff7359c50cea60fe7
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-13 10:52:10 -0300

    roteiro 7

--- roteiro6/meu_grafo_matriz_adjacencia_dir.py
+++ roteiro6/meu_grafo_matriz_adjacencia_dir.py
@@ -1,7 +1,7 @@
 from bibgrafo.grafo_matriz_adj_dir import *
 from bibgrafo.grafo_exceptions import *
 from copy import deepcopy
-
+from math import inf
 
 class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
 
@@ -30,3 +30,89 @@ class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
               matriz[j][k] = ik
 
     return matriz
+
+
+  def vAdjacentes(self, v):
+    adjacentes = []
+
+    for i in range(len(self.M)):
+      for k in range(len(self.M)):
+        if (len(self.M[i][k]) > 0):
+          if (self.N[i] == v):
+            adjacentes.append(self.N[k])
+    return adjacentes
+
+  def drone(self, origem, destino, cargaInicial, cargaMaxima, listaDeCargas = []):
+    '''
+    Provê uma lista aplicando o algotimo de dijkstra modificado
+    :param V: o vertice origem, vertice destino, carga inicial, carga máxima, 
+    lista de vertices com pontos de carga.
+    :return: lista com o menor caminho do grafo
+    :raises: VerticeInvalidoException se o vértice não existe no grafo
+    '''
+    cargaDoDrone = {}
+    pi = {}
+    beta = {}
+    verticesNaoVisitados = []
+    grafo = self.N
+
+    if (origem not in grafo or destino not in grafo):
+      raise VerticeInvalidoException("O vértice não existe no grafo")
+
+    # Adicionando os valores iniciais
+    for v in grafo:
+      beta[v] = inf
+      cargaDoDrone[v] = inf
+      verticesNaoVisitados.append(v)
+    beta[origem] = 0
+
+    if origem in listaDeCargas:
+      cargaDoDrone[origem] = cargaMaxima
+    else:
+      cargaDoDrone[origem] = cargaInicial
+
+    verticesNaoVisitados.remove(origem)
+    w = origem
+    aux = 0
+    arestas = []
+    
+    while w != destino:
+      arestas = self.vAdjacentes(w)
+      for r in arestas:
+        if r in verticesNaoVisitados:
+          if beta[r] >= (beta[w] + 1):
+            beta[r] = (beta[w] + 1)
+            pi[r] = w
+            if r in listaDeCargas:
+              cargaDoDrone[r] = cargaMaxima
+            else:
+              cargaDoDrone[r] = cargaDoDrone[w] - 1
+
+      for i in verticesNaoVisitados:
+        if cargaDoDrone[i] == 0 and i != origem:
+          beta[i] = inf
+
+      menorDosBetas = inf
+
+      for r in verticesNaoVisitados:
+        if beta[r] < menorDosBetas:
+          menorDosBetas = beta[r]
+          r_menorDosBetas = r
+
+      if menorDosBetas == inf:
+        return False
+
+      aux = r_menorDosBetas
+      verticesNaoVisitados.remove(aux)
+      w = aux
+
+    menorCaminho = []
+
+    menorCaminho.append(destino)
+
+    while destino != origem:
+      menorCaminho.append(pi[destino])
+      destino = pi[destino]
+
+    menorCaminho.reverse()
+    return menorCaminho
--- roteiro6/teste.py
+++ roteiro6/teste.py
@@ -1,12 +1,21 @@
-from meu_grafo_matriz_adjacencia_dir import *
+from bibgrafo.grafo_matriz_adj_dir import GrafoMatrizAdjacenciaDirecionado
 
-g_v1 = MeuGrafo(['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8'])
-g_v1.adicionaAresta('a1', 'V1', 'V2')
-g_v1.adicionaAresta('a2', 'V2', 'V3')
-g_v1.adicionaAresta('a3', 'V3', 'V4')
-g_v1.adicionaAresta('a4', 'V4', 'V5')
-g_v1.adicionaAresta('a5', 'V5', 'V6')
-g_v1.adicionaAresta('a6', 'V7', 'V8')
+from meu_grafo_matriz_adjacencia_dir import MeuGrafo
+dijkstraDoRoteiro = MeuGrafo(['V1','V2','V3','V4','V5','V6','V7','V8','V9','V10',
+'V11','V12','V13'])
+dijkstraDoRoteiro.adicionaAresta('a1', 'V1', 'V4')
+dijkstraDoRoteiro.adicionaAresta('a2', 'V2', 'V5')
+dijkstraDoRoteiro.adicionaAresta('a3', 'V1', 'V3')
+dijkstraDoRoteiro.adicionaAresta('a4', 'V1', 'V2')
+dijkstraDoRoteiro.adicionaAresta('a5', 'V3', 'V6')
+dijkstraDoRoteiro.adicionaAresta('a6', 'V4', 'V12')
+dijkstraDoRoteiro.adicionaAresta('a7', 'V5', 'V9')
+dijkstraDoRoteiro.adicionaAresta('a8', 'V4', 'V8')
+dijkstraDoRoteiro.adicionaAresta('a9', 'V2', 'V3')
+dijkstraDoRoteiro.adicionaAresta('a10', 'V5', 'V6')
+dijkstraDoRoteiro.adicionaAresta('a11', 'V6', 'V10')
+dijkstraDoRoteiro.adicionaAresta('a12', 'V6', 'V11')
+dijkstraDoRoteiro.adicionaAresta('a13', 'V6', 'V7')
 
-print(g_v1.warshall())
 
+print(dijkstraDoRoteiro.drone('V1', 'V9', 3, 3, ['V5']))
