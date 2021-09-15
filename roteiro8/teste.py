d8df0bb5eee455792478206c2bef214c4523f588
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-15 12:34:39 -0300

    adicionando testes

--- roteiro8/teste.py
+++ roteiro8/teste.py
@@ -1,15 +1,15 @@
 from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
 from meu_grafo import *
 
-grafo_roteiro = MeuGrafo(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
-grafo_roteiro.adicionaAresta('a1', 'A', 'B', 1)
-grafo_roteiro.adicionaAresta('a2', 'B', 'C', 2)
-grafo_roteiro.adicionaAresta('a3', 'C', 'G', 2)
-grafo_roteiro.adicionaAresta('a4', 'G', 'F', 3)
-grafo_roteiro.adicionaAresta('a5', 'E', 'F', 4)
-grafo_roteiro.adicionaAresta('a6', 'E', 'G', 1)
-grafo_roteiro.adicionaAresta('a7', 'E', 'D', 2)
-grafo_roteiro.adicionaAresta('a8', 'A', 'D', 3)
-grafo_roteiro.adicionaAresta('a9', 'B', 'G', 5)
-grafo_roteiro.adicionaAresta('a10', 'D', 'G', 3)
-print(grafo_roteiro.prim())
+g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+g_p.adicionaAresta('a1', 'J', 'C', 5)
+g_p.adicionaAresta('a2', 'C', 'E', 3)
+g_p.adicionaAresta('a3', 'C', 'E', 4)
+g_p.adicionaAresta('a4', 'P', 'C', 2)
+g_p.adicionaAresta('a5', 'P', 'C', 1)
+g_p.adicionaAresta('a6', 'T', 'C', 5)
+g_p.adicionaAresta('a7', 'M', 'C', 7)
+g_p.adicionaAresta('a8', 'M', 'T', 8)
+g_p.adicionaAresta('a9', 'T', 'Z', 9)
+
+print(g_p.prim())
